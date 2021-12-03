a88df862644585ae364ce47454680ceab7ebc2d4
Andrii Denysenko <andrey2850@gmail.com>
2021-12-03 09:55:00 -0500

    Add xamarin auth

---
+++ XamarinFirebaseDemo/XamarinFirebaseDemo/XamarinFirebaseDemo/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFirebaseDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        IAuth auth;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+        }
+
+        async void LoginClicked(object sender, EventArgs e)
+        {
+            var success = await auth.LoginWithEmailPassword(EmailInput.Text, PasswordInput.Text);
+            if (success)
+            {
+                await Navigation.PushAsync(new Home());
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        async private void ShowError()
+        {
+            await DisplayAlert("Authentication Failed", "E-mail or password are incorrect. Try again!", "OK");
+        }
+
+        async void SignUpClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+    }
+}
---
+++ XamarinFirebaseDemo/XamarinFirebaseDemo/XamarinFirebaseDemo/SignUp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFirebaseDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUp : ContentPage
+    {
+        IAuth auth;
+
+        public SignUp()
+        {
+            InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+        }
+
+        async void SignUpClicked(object sender, EventArgs e)
+        {
+            bool created = await auth.SignUpWithEmailPassword(EmailInput.Text, PasswordInput.Text);
+            if (created)
+            {
+                await DisplayAlert("Success", "Welcome to our system. Log in to have full access", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                await DisplayAlert("Sign Up Failed", "Something went wrong. Try again!", "OK");
+            }
+        }
+
+        async void LogInClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
